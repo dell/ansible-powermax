132d05fd06010b04a7ccc56e8361d72b245d479f
Tao He <tao.he1@dell.com>
2025-01-27 06:10:49 +0000

    Improve coverage for masking view

--- tests/unit/plugins/modules/test_maskingview.py
+++ tests/unit/plugins/modules/test_maskingview.py
@@ -9,7 +9,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import pytest
-from mock.mock import MagicMock
+from mock.mock import MagicMock, patch
 from ansible_collections.dellemc.powermax.tests.unit.plugins.module_utils.mock_maskingview_api import MockMaskingViewApi
 from ansible_collections.dellemc.powermax.plugins.module_utils.storage.dell \
     import utils
@@ -53,21 +53,73 @@ class TestMaskingView():
             maskingview_module_mock.provisioning.get_host_list = \
                 MagicMock(side_effect=Exception)
 
-    def mock_get_masking_view_list(self, maskingview_module_mock):
+    def mock_get_masking_view_list(self, maskingview_module_mock, call_exception):
+        if not call_exception:
+            maskingview_module_mock.provisioning.get_masking_view_list = \
+                MagicMock(return_value=MockMaskingViewApi.MASKING_VIEW_LIST_API_RESPONSE)
+        else:
+            maskingview_module_mock.provisioning.get_masking_view_list = \
+                MagicMock(side_effect=Exception)
+
+    def mock_get_masking_view_list_exist(self, maskingview_module_mock):
         maskingview_module_mock.provisioning.get_masking_view_list = \
-            MagicMock(return_value=MockMaskingViewApi.MASKING_VIEW_LIST_API_RESPONSE)
+            MagicMock(return_value=MockMaskingViewApi.MASKING_VIEW_LIST_API_RESPONSE_EXIST)
 
-    def mock_create_mv_exist_comp(self, maskingview_module_mock, type):
-        maskingview_module_mock.provisioning.create_masking_view_existing_components = \
-            MagicMock(return_value=MockMaskingViewApi.get_create_mv_payload(type))
+    def mock_get_masking_view_detail(self, maskingview_module_mock, key, value):
+        maskingview_module_mock.provisioning.get_masking_view = \
+            MagicMock(return_value=MockMaskingViewApi.get_mv_detail_api_response(key, value))
 
-    def create_mv_host(self, maskingview_module_mock, type, call_exception=False):
+    def mock_create_mv_exist_comp(self, maskingview_module_mock, type, call_exception):
+        if not call_exception:
+            maskingview_module_mock.provisioning.create_masking_view_existing_components = \
+                MagicMock(return_value=MockMaskingViewApi.get_create_mv_payload(type))
+        else:
+            maskingview_module_mock.provisioning.create_masking_view_existing_components = \
+                MagicMock(side_effect=Exception)
+
+    def mock_delete_masking_view(self, maskingview_module_mock, call_exception):
+        if not call_exception:
+            maskingview_module_mock.provisioning.delete_masking_view = \
+                MagicMock(return_value=True)
+        else:
+            maskingview_module_mock.provisioning.delete_masking_view = \
+                MagicMock(side_effect=Exception)
+
+    def mock_rename_masking_view(self, maskingview_module_mock, call_exception):
+        if not call_exception:
+            maskingview_module_mock.provisioning.rename_masking_view = \
+                MagicMock(return_value=True)
+        else:
+            maskingview_module_mock.provisioning.rename_masking_view = \
+                MagicMock(side_effect=Exception)
+
+    def create_mv_host(self, maskingview_module_mock, type, call_list_exception=False, call_create_exception=False):
         if type == 'host':
-            self.mock_host_list(maskingview_module_mock, call_exception)
+            self.mock_host_list(maskingview_module_mock, call_list_exception)
         elif type == 'hostgroup':
-            self.mock_hostgroup_list(maskingview_module_mock, call_exception)
-        self.mock_get_masking_view_list(maskingview_module_mock)
-        self.mock_create_mv_exist_comp(maskingview_module_mock, type)
+            self.mock_hostgroup_list(maskingview_module_mock, call_list_exception)
+        self.mock_get_masking_view_list(maskingview_module_mock, call_list_exception)
+        self.mock_create_mv_exist_comp(maskingview_module_mock, type, call_create_exception)
+        maskingview_module_mock.perform_module_operation()
+
+    def change_mv_host(self, maskingview_module_mock, key, value, call_exception=False):
+        self.mock_host_list(maskingview_module_mock, call_exception)
+        self.mock_get_masking_view_list_exist(maskingview_module_mock)
+        self.mock_get_masking_view_detail(maskingview_module_mock, key, value)
+        maskingview_module_mock.perform_module_operation()
+
+    def delete_mv_host(self, maskingview_module_mock, call_exception=False):
+        self.mock_host_list(maskingview_module_mock, call_exception)
+        self.mock_get_masking_view_list_exist(maskingview_module_mock)
+        self.mock_get_masking_view_detail(maskingview_module_mock, '', '')
+        self.mock_delete_masking_view(maskingview_module_mock, call_exception)
+        maskingview_module_mock.perform_module_operation()
+
+    def rename_mv_host(self, maskingview_module_mock, call_exception=False):
+        self.mock_host_list(maskingview_module_mock, False)
+        self.mock_get_masking_view_list_exist(maskingview_module_mock)
+        self.mock_get_masking_view_detail(maskingview_module_mock, '', '')
+        self.mock_rename_masking_view(maskingview_module_mock, call_exception)
         maskingview_module_mock.perform_module_operation()
 
     def test_create_maskingview_with_host(self, maskingview_module_mock):
@@ -95,3 +147,94 @@ class TestMaskingView():
         self.create_mv_host(maskingview_module_mock, 'hostgroup', True)
         assert MockMaskingViewApi.get_create_mv_exception_response('hostgroup') == \
             maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_create_maskingview_with_hostgroup_create_exception(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload('hostgroup'))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.create_mv_host(maskingview_module_mock, 'hostgroup', False, True)
+        assert MockMaskingViewApi.get_create_mv_create_exception_response('test_mv') == \
+            maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_create_maskingview_with_both_hostgroup_host_exception(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload('hostgroup'))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload('host'))
+        maskingview_module_mock.module.params.update(self.get_module_args)
+        self.create_mv_host(maskingview_module_mock, 'hostgroup')
+        assert MockMaskingViewApi.get_create_mv_both_host_hostgroup_exception_response('test_mv') == \
+            maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_create_maskingview_with_none_hostgroup_host_exception(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload(''))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.create_mv_host(maskingview_module_mock, 'hostgroup')
+        assert MockMaskingViewApi.get_create_mv_none_host_hostgroup_exception_response('test_mv') == \
+            maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_change_maskingview_with_unsupported_exception(self, maskingview_module_mock):
+        unsupported_params = ['portGroupId', 'storageGroupId', 'hostId', 'hostGroupId', 'hostId_hostgroup_name', 'hostGroupId_host_name']
+        for param_key in unsupported_params:
+            self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload('host'))
+            if param_key == 'hostId_hostgroup_name':
+                self.get_module_args.update(MockMaskingViewApi.get_create_mv_payload('hostgroup'))
+            maskingview_module_mock.module.params = self.get_module_args
+            if param_key != 'hostId_hostgroup_name' and param_key != 'hostId_hostgroup_name' :
+                self.change_mv_host(maskingview_module_mock, param_key, ('%s_change' % param_key))
+            assert MockMaskingViewApi.get_change_mv_exception_response('test_mv') == \
+                maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_delete_maskingview(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_delete_mv_payload())
+        maskingview_module_mock.module.params = self.get_module_args
+        self.delete_mv_host(maskingview_module_mock)
+        maskingview_module_mock.provisioning.delete_masking_view.assert_called()
+
+    def test_delete_maskingview_with_check_mode(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_delete_mv_payload())
+        maskingview_module_mock.module.params = self.get_module_args
+        maskingview_module_mock.module.check_mode = True
+        self.delete_mv_host(maskingview_module_mock)
+        maskingview_module_mock.provisioning.delete_masking_view.assert_not_called()
+
+    def test_delete_maskingview_with_exception(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_delete_mv_payload())
+        maskingview_module_mock.module.params = self.get_module_args
+        self.delete_mv_host(maskingview_module_mock, True)
+        assert MockMaskingViewApi.get_delete_mv_exception_response('test_mv') == \
+            maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_rename_maskingview(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_rename_mv_payload('test_mv_rename'))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.rename_mv_host(maskingview_module_mock)
+        maskingview_module_mock.provisioning.rename_masking_view.assert_called()
+
+    def test_rename_maskingview_with_check_mode(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_rename_mv_payload('test_mv_rename'))
+        maskingview_module_mock.module.params = self.get_module_args
+        maskingview_module_mock.module.check_mode = True
+        self.rename_mv_host(maskingview_module_mock)
+        maskingview_module_mock.provisioning.rename_masking_view.assert_not_called()
+
+    def test_rename_maskingview_with_same_name(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_rename_mv_payload('test_mv'))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.rename_mv_host(maskingview_module_mock)
+        maskingview_module_mock.provisioning.rename_masking_view.assert_not_called()
+
+    def test_rename_maskingview_with_exception(self, maskingview_module_mock):
+        self.get_module_args.update(MockMaskingViewApi.get_rename_mv_payload('test_mv_rename'))
+        maskingview_module_mock.module.params = self.get_module_args
+        self.rename_mv_host(maskingview_module_mock, True)
+        assert MockMaskingViewApi.get_rename_mv_exception_response('test_mv') == \
+            maskingview_module_mock.module.fail_json.call_args[1]['msg']
+
+    def test_maskingview_init_check_failed(self, maskingview_module_mock):
+        with patch('ansible_collections.dellemc.powermax.plugins.modules.maskingview.HAS_PYU4V', False):
+            with patch('ansible_collections.dellemc.powermax.plugins.modules.maskingview.PYU4V_VERSION_CHECK', "mock check err"):
+                utils.universion_check = MagicMock(return_value={
+                    "is_valid_universion": False, "user_message": "mock user msg"})
+                utils.get_U4V_connection = MagicMock(side_effect=Exception)
+                maskingview_module_mock.show_error_exit = MagicMock()
+                maskingview_module_mock.__init__()
+                assert maskingview_module_mock.show_error_exit.call_count == 4
