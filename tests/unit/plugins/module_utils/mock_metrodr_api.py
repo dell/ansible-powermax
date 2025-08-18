ea01540a0deea884611d7f3c7ea5b5141a8cc1fa
Shekhar Joshi <Shekhar.Joshi@dell.com>
2025-08-18 09:51:50 -0400

    Increase Unit Test Coverage- copyright block update

--- tests/unit/plugins/module_utils/mock_metrodr_api.py
+++ tests/unit/plugins/module_utils/mock_metrodr_api.py
@@ -1,9 +1,9 @@
-# Copyright: (c) 2022-2025, Dell Technologies
+# Copyright: (c) 2025, Dell Technologies
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
-Mock Info Api for Info Test module on PowerMax
+Mock MetroDR Api for MetroDR Test module on PowerMax
 """
 
 from __future__ import (absolute_import, division, print_function)
